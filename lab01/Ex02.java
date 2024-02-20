1369b896f03d54cd5cbab6dee874996311267bb6
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-02-20 20:38:02 +0000

    Parse composed numbers-based 2nd expression

--- lab01/Ex02.java
+++ lab01/Ex02.java
@@ -6,10 +6,11 @@ public class Ex02 {
     public static void main(String[] args) {
         Scanner sInScanner = new Scanner(System.in);
         Map<String, Double> variables = new HashMap<>();
+        System.out.print(">>> ");
 
         while (sInScanner.hasNextLine()) {
             String exp1, exp2, token;
-            double result;
+            double num2, result;
 
             Scanner sc = new Scanner(sInScanner.nextLine());
 
@@ -40,18 +41,23 @@ public class Ex02 {
 
             // Expression 2
             if (!sc.hasNext()) {
-                System.err.println("Expression 2 is not valid!");
+                System.err.println("Expression 2 doesn't exist!");
                 continue;
             }
-            exp2 = sc.next();
+            exp2 = sc.nextLine();
+
+            // Evaluate expression 2
+            num2 = parseSecondExpression(exp2);
+            if (Double.isNaN(num2)) {
+                continue;
+            }
 
             // Calculation
-            if (token.equals("=")) {
-                variables.put(exp1, Double.parseDouble(exp2));
-                System.out.println(exp2);
+            if (token.equals("=") && exp1.matches("[A-Za-z]*")) {
+                variables.put(exp1, num2);
+                System.out.println(num2);
             } else {
                 double num1 = Double.parseDouble(exp1);
-                double num2 = Double.parseDouble(exp2);
                 result = switch (token) {
                     case "-" -> num1 - num2;
                     case "*" -> num1 * num2;
@@ -60,6 +66,45 @@ public class Ex02 {
                 };
                 System.out.println(result);
             }
+
+            System.out.print(">>> ");
         }
+    }
+
+    private static double parseSecondExpression(String expr) {
+        Scanner sc = new Scanner(expr);
+
+        if (!sc.hasNextDouble()) {
+            System.err.println("Expression 2 is invalid!");
+            return Double.NaN;
+        }
+        double num1 = sc.nextDouble();
+
+        if (!sc.hasNext()) {
+            return num1;
+        }
+
+        if (!sc.hasNext("[-+*/]")) {
+            System.err.println("Token after expression 2 is invalid!");
+            return Double.NaN;
+        }
+        String token = sc.next();
+
+        if (!sc.hasNextDouble()) {
+            System.err.println("Expression 3 is invalid!");
+            return Double.NaN;
+        }
+        double num2 = sc.nextDouble();
+
+        if (sc.hasNext()) {
+            System.out.println("Warning: Expression has more elements than supposed! Truncating.");
+        }
+
+        return switch (token) {
+            case "-" -> num1 - num2;
+            case "*" -> num1 * num2;
+            case "/" -> num1 / num2;
+            default -> num1 + num2;
+        };
     }
 }
