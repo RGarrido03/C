99490d52bdd6be91e8e0888b4f971b4de20e4159
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-03-19 12:45:31 +0000

    Exercise 6

--- lab02/b2_06/Calculator.g4
+++ lab02/b2_06/Calculator.g4
@@ -2,15 +2,19 @@ grammar Calculator;
 
 program: stat* EOF;
 
-stat: expr? NEWLINE;
+stat: (assignment | expr)? NEWLINE;
+
+assignment: ID '=' expr;
 
 expr:
 	expr op = ('*' | '/' | '%') expr	# ExprMultDivMod
 	| expr op = ('+' | '-') expr		# ExprAddSub
 	| Integer							# ExprInteger
+	| ID                                # ExprId
 	| '(' expr ')'						# ExprParent;
 
 Integer: [0-9]+;
 NEWLINE: '\r'? '\n';
 WS: [ \t]+ -> skip;
 COMMENT: '#' .*? '\n' -> skip;
+ID : [a-zA-Z_]+;
--- lab02/b2_06/Interpreter.java
+++ lab02/b2_06/Interpreter.java
@@ -1,6 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
+
 // Generated from Calculator.g4 by ANTLR 4.13.1
 @SuppressWarnings("CheckReturnValue")
 public class Interpreter extends CalculatorBaseVisitor<Integer> {
+    private final Map<String, Integer> variables = new HashMap<>();
 
     @Override
     public Integer visitProgram(CalculatorParser.ProgramContext ctx) {
@@ -9,8 +13,20 @@ public class Interpreter extends CalculatorBaseVisitor<Integer> {
 
     @Override
     public Integer visitStat(CalculatorParser.StatContext ctx) {
-        Integer res = visit(ctx.expr());
+        Integer res = visit(ctx.expr() != null ? ctx.expr() : ctx.assignment());
+        if (res == null) {
+            return null;
+        }
         System.out.println(">> " + res);
+        return res;
+    }
+
+    @Override
+    public Integer visitAssignment(CalculatorParser.AssignmentContext ctx) {
+        String id = ctx.ID().getText();
+        Integer res = visit(ctx.expr());
+        variables.put(id, res);
+
         return res;
     }
 
@@ -18,11 +34,17 @@ public class Interpreter extends CalculatorBaseVisitor<Integer> {
     public Integer visitExprAddSub(CalculatorParser.ExprAddSubContext ctx) {
         Integer n1 = visit(ctx.expr(0));
         Integer n2 = visit(ctx.expr(1));
+
+        if (n1 == null || n2 == null) {
+            return null;
+        }
+
         String operation = ctx.op.getText();
 
         return switch (operation) {
+            case "+" -> n1 + n2;
             case "-" -> n1 - n2;
-            default -> n1 + n2;
+            default -> throw new IllegalStateException("Unexpected operation: " + operation);
         };
     }
 
@@ -37,15 +59,31 @@ public class Interpreter extends CalculatorBaseVisitor<Integer> {
     }
 
     @Override
+    public Integer visitExprId(CalculatorParser.ExprIdContext ctx) {
+        String id = ctx.ID().getText();
+        if (variables.containsKey(id)) {
+            return variables.get(id);
+        }
+        System.err.println("Variable " + id + " not found");
+        return null;
+    }
+
+    @Override
     public Integer visitExprMultDivMod(CalculatorParser.ExprMultDivModContext ctx) {
         Integer n1 = visit(ctx.expr(0));
         Integer n2 = visit(ctx.expr(1));
+
+        if (n1 == null || n2 == null) {
+            return null;
+        }
+
         String operation = ctx.op.getText();
 
         return switch (operation) {
+            case "*" -> n1 * n2;
             case "/" -> n1 / n2;
             case "%" -> n1 % n2;
-            default -> n1 * n2;
+            default -> throw new IllegalStateException("Unexpected operation: " + operation);
         };
     }
 }
